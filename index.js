90fee48a28cd9305c7003cf77edc47cb49d64d2c
rir7890 <Rahul Kumar>
2024-03-15 09:24:49 +0530

    angela secret reveal website

---
+++ index.js
@@ -0,0 +1,36 @@
+// HINTS:
+// 1. Import express and axios
+import express from "express";
+import axios from "axios";
+// 2. Create an express app and set the port number.
+const app = express();
+const PORT = 4000;
+app.set("views engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 3. Use the public folder for static files.
+
+app.use(express.static("public"));
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
+
+// 6. Listen on your predefined port and start the server.
+app.listen(PORT, () => {
+  console.log(`App listening on ${PORT}`);
+});
